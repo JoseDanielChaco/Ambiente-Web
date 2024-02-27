16632220a5288ca34e032cebfbf9e4b97df69e6d
juandramvar <118952208+juandramvar@users.noreply.github.com>
2024-02-27 00:39:25 -0600

    LoginUpdate2

--- editar_usuario.php
+++ editar_usuario.php
@@ -44,7 +44,6 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar Usuario</title>
-    
 </head>
 <body>
     <h1>Editar Usuario</h1>
