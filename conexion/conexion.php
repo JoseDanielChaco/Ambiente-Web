c85dd8255506dfffde550893d152a80bbedea651
Noé Rojas Rodríguez <nrojas70164@ufide.ac.cr>
2024-04-16 23:25:39 -0600

    Commit
    Cambio en contraseña de conexion

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,7 +1,7 @@
 <?php
     $host = "localhost";
     $user = "root";
-    $clave = "NoE.021!";
+    $clave = "";
     $bd = "proyecto";
     $conexion = mysqli_connect($host,$user,$clave,$bd);
     if (mysqli_connect_errno()){
