6f3bdbfe138bf19c136a634d74c1bf545931d637
Noé Rojas Rodríguez <nrojas70164@ufide.ac.cr>
2024-04-17 17:02:26 -0600

    Commit

--- guardar_favorito.php
+++ guardar_favorito.php
@@ -1,14 +1,12 @@
 <?php
 // Favoritos
-// Verificar si se recibió una solicitud AJAX
+
 if (isset($_POST['action']) && $_POST['action'] === 'favoritos' && isset($_POST['data'])) {
-    // Iniciar o reanudar la sesión
+
     session_start();
     
-    // Obtener el ID del vehículo a agregar al carrito
     $vehicleId = $_POST['data'][0];
 
-    // Verificar si el carrito ya existe en la sesión
     if (!isset($_SESSION['favoritos'])) {
         // Si no existe, crear un nuevo array para el carrito
         $_SESSION['favoritos'] = array();
@@ -17,14 +15,12 @@ if (isset($_POST['action']) && $_POST['action'] === 'favoritos' && isset($_POST[
     // Agregar el ID del vehículo al carrito
     $_SESSION['favoritos'][] = $vehicleId;
 
-    // Enviar una respuesta JSON indicando que se agregó el vehículo al carrito
     $response = array(
         'success' => true,
-        'total' => count($_SESSION['favoritos']) // Se puede enviar el número total de elementos en el carrito
+        'total' => count($_SESSION['favoritos']) 
     );
     echo json_encode($response);
 } else {
-    // Si la solicitud no es válida, enviar una respuesta JSON con error
     $response = array(
         'success' => false,
         'message' => 'Solicitud no válida'
